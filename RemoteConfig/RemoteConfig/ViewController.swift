b826ea115775fd45a17018dfb9c35ad65506a46f
Alok Sahay <alok.sahay87@gmail.com>
2024-10-09 19:13:14 +0200

    Recreated project

---
+++ RemoteConfig/RemoteConfig/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RemoteConfig
+//
+//  Created by Alok Sahay on 09.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
