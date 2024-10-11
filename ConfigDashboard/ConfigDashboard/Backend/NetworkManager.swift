fd6e6ca598325e717ed47e03cf521e75cd754cdf
Alok Sahay <alok.sahay87@gmail.com>
2024-10-11 13:38:58 +0200

    Generate signed URL for the DB

--- ConfigDashboard/ConfigDashboard/Backend/NetworkManager.swift
+++ ConfigDashboard/ConfigDashboard/Backend/NetworkManager.swift
@@ -9,11 +9,16 @@ import Foundation
 
 class NetworkManager {
     
-    static var pinataUploadEndpoint: String = "https://uploads.pinata.cloud/v3/files"
-    static var pinataGetFilesEndpoint: String = "https://api.pinata.cloud/v3/files"
     static let sharedManager = NetworkManager()
     
-    var databaseLocation: ConfigFileData?
+    static var pinataUploadEndpoint: String = "https://uploads.pinata.cloud/v3/files"
+    static var pinataFilesEndpoint: String = "https://api.pinata.cloud/v3/files"
+    static var pinataGatewayEndpoint: String = "https://cyan-genetic-barracuda-339.mypinata.cloud/files/"
+    static let urlDuration = 500000  //duration of signed URL
+    
+    var remoteDatabaseState: ConfigFileData?
+    var dataSource: ConfigDatabase?
+    var databaseURLEndpoint: String?
     
     static var bearerToken: String {
         return ProcessInfo.processInfo.environment["PinataToken"] ?? "PINATA_JWT_TOKEN" // add you JWT here (not safe to save it in the client though).
@@ -28,62 +33,62 @@ class NetworkManager {
     
     
     
-//    func fetchConfigById(fileId: String, completion: @escaping (Result<AppConfig, Error>) -> Void) {
-//        
-//        let fetchFileByName = NetworkManager.pinataGetFilesEndpoint + "/\(fileId)"
-//        
-//        guard let url = URL(string: fetchFileByName) else {
-//            print("Invalid URL.")
-//            
-//            let error = NSError(
-//                domain: "api.pinata.cloud",
-//                code: 999,
-//                userInfo: [NSLocalizedDescriptionKey: "Invalid URL"]
-//            )            
-//            completion(.failure(error))
-//            return
-//        }
-//        
-//        var request = URLRequest(url: url)
-//        request.httpMethod = "GET"
-//        request.setValue("Bearer \(NetworkManager.bearerToken)", forHTTPHeaderField: "Authorization")
-//        
-//        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-//            if let error = error {
-//                print("Error: \(error.localizedDescription)")
-//                return
-//            }
-//            
-//            if let httpResponse = response as? HTTPURLResponse {
-//                print("HTTP Status Code: \(httpResponse.statusCode)")
-//            }
-//            
-//            if let data = data {
-//                
-//                do {
-//                    let responseString = String(data: data, encoding: .utf8)
-//                    print("Response: \(responseString ?? "No response data")")
-//                    
-//                    let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
-//                    
-////                    if let dataObject = json?["data"] as? [String: Any],
-////                       let filesArray = dataObject["files"] as? [[String: Any]] {
-////                        let filesData = try JSONSerialization.data(withJSONObject: filesArray, options: [])
-////                        let files = try JSONDecoder().decode([ConfigFileData].self, from: filesData)
-////                        completion(.success(files))
-////                    }
-//                    
-//                } catch {
-//                    completion(.failure(error))
-//                }
-//            }
-//        }
-//        task.resume()
-//    }
+    //    func fetchConfigById(fileId: String, completion: @escaping (Result<AppConfig, Error>) -> Void) {
+    //
+    //        let fetchFileByName = NetworkManager.pinataGetFilesEndpoint + "/\(fileId)"
+    //
+    //        guard let url = URL(string: fetchFileByName) else {
+    //            print("Invalid URL.")
+    //
+    //            let error = NSError(
+    //                domain: "api.pinata.cloud",
+    //                code: 999,
+    //                userInfo: [NSLocalizedDescriptionKey: "Invalid URL"]
+    //            )
+    //            completion(.failure(error))
+    //            return
+    //        }
+    //
+    //        var request = URLRequest(url: url)
+    //        request.httpMethod = "GET"
+    //        request.setValue("Bearer \(NetworkManager.bearerToken)", forHTTPHeaderField: "Authorization")
+    //
+    //        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+    //            if let error = error {
+    //                print("Error: \(error.localizedDescription)")
+    //                return
+    //            }
+    //
+    //            if let httpResponse = response as? HTTPURLResponse {
+    //                print("HTTP Status Code: \(httpResponse.statusCode)")
+    //            }
+    //
+    //            if let data = data {
+    //
+    //                do {
+    //                    let responseString = String(data: data, encoding: .utf8)
+    //                    print("Response: \(responseString ?? "No response data")")
+    //
+    //                    let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+    //
+    ////                    if let dataObject = json?["data"] as? [String: Any],
+    ////                       let filesArray = dataObject["files"] as? [[String: Any]] {
+    ////                        let filesData = try JSONSerialization.data(withJSONObject: filesArray, options: [])
+    ////                        let files = try JSONDecoder().decode([ConfigFileData].self, from: filesData)
+    ////                        completion(.success(files))
+    ////                    }
+    //
+    //                } catch {
+    //                    completion(.failure(error))
+    //                }
+    //            }
+    //        }
+    //        task.resume()
+    //    }
     
     func fetchDatabaseState(completion: @escaping (Bool, Error?) -> Void) {
         
-        let fetchFilesByGroupId = NetworkManager.pinataGetFilesEndpoint + "?group=\(NetworkManager.configGroupId)"
+        let fetchFilesByGroupId = NetworkManager.pinataFilesEndpoint + "?group=\(NetworkManager.configGroupId)"
         
         guard let url = URL(string: fetchFilesByGroupId) else {
             print("Invalid URL.")
@@ -126,21 +131,25 @@ class NetworkManager {
                         let files = try JSONDecoder().decode([ConfigFileData].self, from: filesData)
                         
                         if let dbLocation = files.first(where: { $0.name == "AppConfig"}) {
-                            self.databaseLocation = dbLocation
+                            self.remoteDatabaseState = dbLocation
                         }
                         completion(true, nil)
                     }
                     
                 } catch {
                     completion(false, error)
+                    return
                 }
             }
-        }            
+        }
         task.resume()
     }
     
-    
-    func uploadConfig(config: AppConfig, completion: @escaping (Result<ConfigFileData, Error>) -> Void) {
+    func uploadDatabase(completion: @escaping (Bool, Error?) -> Void) {
+        
+        if (dataSource == nil) {
+            dataSource = ConfigDatabase(configurations: [])
+        }
         
         guard let url = URL(string: NetworkManager.pinataUploadEndpoint) else {
             print("Invalid URL.")
@@ -151,19 +160,12 @@ class NetworkManager {
                 userInfo: [NSLocalizedDescriptionKey: "Invalid URL"]
             )
             
-            completion(.failure(error))
+            completion(false,error)
             return
         }
         
         do {
-            
-            guard let jsonString = config.encodeToJSON(), let jsonData = jsonString.data(using: .utf8) else {
-                print("Failed to convert config to JSON.")
-                return
-            }
-            
-            print("JSON string: " + jsonString)
-            print("JSON config: " + (String(data: jsonData, encoding: .utf8) ?? ""))
+            let jsonData = try JSONEncoder().encode(dataSource)
             let boundary = "Boundary-\(UUID().uuidString)"
             
             var request = URLRequest(url: url)
@@ -178,7 +180,7 @@ class NetworkManager {
             let task = URLSession.shared.dataTask(with: request) { data, response, error in
                 
                 if let error = error {
-                    completion(.failure(error))
+                    completion(false, error)
                     return
                 }
                 
@@ -188,25 +190,101 @@ class NetworkManager {
                         print("Response: \(responseString ?? "No response data")")
                         
                         let receivedConfig = try JSONDecoder().decode([String: ConfigFileData].self, from: data)
-                        
                         if let fileData = receivedConfig["data"] {
-                            completion(.success(fileData))
+                            
+                            // saved location of DB
+                            self.remoteDatabaseState = fileData
+                            completion(true, nil)
                         } else {
                             let error = NSError(
                                 domain: "api.pinata.cloud",
                                 code: 909,
                                 userInfo: [NSLocalizedDescriptionKey: "Parse error"]
                             )
-                            completion(.failure(error))
+                            completion(false, error)
+                            return
                         }
                         
                     } catch {
-                        completion(.failure(error))
+                        completion(false, error)
                     }
                 }
             }
             task.resume()
+            
+        } catch {
+            completion(false, error)
         }
     }
     
+    func fetchDB(completion: @escaping (Bool, Error?) -> Void) {
+        
+        
+        
+    }
+    
+    func getDatabaseSignedURL(completion: @escaping (Bool, Error?) -> Void) {
+        
+        guard let requestUrl = URL(string: NetworkManager.pinataFilesEndpoint + "/sign") else {
+            print("Invalid URL.")
+            
+            let error = NSError(
+                domain: "api.pinata.cloud",
+                code: 999,
+                userInfo: [NSLocalizedDescriptionKey: "Invalid URL"]
+            )
+            
+            completion(false,error)
+            return
+        }
+        
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = "POST"
+        
+        request.setValue("Bearer \(NetworkManager.bearerToken)", forHTTPHeaderField: "Authorization")
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let bodyParams: [String: Any] = [
+            "url": NetworkManager.pinataGatewayEndpoint,
+            "date": Date().timeIntervalSince1970, //now
+            "expires": NetworkManager.urlDuration,
+            "method": "GET"
+        ]
+        
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: bodyParams, options: [])
+            request.httpBody = jsonData
+        } catch {
+            print("Error serializing JSON: \(error)")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("Error: \(error)")
+                completion(false,error)
+                return
+            }
+            
+            if let data = data {
+                do {
+                    let responseString = String(data: data, encoding: .utf8)
+                    print("Response: \(responseString ?? "No response data")")
+                    
+                    if let jsonDict = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+                       let urlString = jsonDict["data"] as? String {
+                        self.databaseURLEndpoint = urlString
+                        completion(true,nil)
+                    } else {
+                        print("Failed to extract URL from the response.")
+                        completion(false, error)
+                    }
+                } catch {
+                    completion(false, error)
+                }
+                
+            }
+        }
+        task.resume()
+    }
 }
--- ConfigDashboard/ConfigDashboard/ViewController.swift
+++ ConfigDashboard/ConfigDashboard/ViewController.swift
@@ -12,58 +12,43 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        //        uploadtestConfig()
-        //        fetchAllConfigs()
-        //        fetchConfigDetail()
-        fetchConfigDatabase()
+        fetchDatabaseState()
     }
     
-    func fetchConfigDetail() { //} (config: ConfigFileData) {
+    func fetchDatabaseState() {
         
-        //        NetworkManager.sharedManager.fetchConfigById(fileId: "019277e0-74f5-7537-abf7-1963ffd96660") { result in
-        //            switch result {
-        //            case .success(let config):
-        //                print("Fetched config: \(config)")
-        //            case .failure(let error):
-        //                print("Failed to fetch config: \(error)")
-        //            }
-        //        }
-    }
-    
-    func uploadtestConfig() {
-        
-        var config = AppConfig()
-        config.appVersion = "2.1"
-        config.apiRootURL = "https://api.example.com"
-        config.apiVersion = "1.0.0"
-        
-        NetworkManager.sharedManager.uploadConfig(config: config) { result in
-            switch result {
-            case .success(let receivedFileData):
-                print("Uploaded successfully, received response:")
-                print("File Location: \(receivedFileData.cid)")
-            case .failure(let error):
-                print("Failed to upload config: \(error)")
-            }
-        }
-    }
-    
-    func fetchConfigDatabase() {
-        NetworkManager.sharedManager.fetchDatabaseState { (success, error) in
+        NetworkManager.sharedManager.fetchDatabaseState { [weak self] (success, error) in
+            
             if success == false {
-                self.showAlert(message: error?.localizedDescription ?? "Unknown error")
+                DispatchQueue.main.async {
+                    self?.showAlert(message: error?.localizedDescription ?? "Unknown error")
+                }
                 return
             }
             
-            if NetworkManager.sharedManager.databaseLocation?.cid == nil {
-                self.showNoDBAlert()
+            if NetworkManager.sharedManager.remoteDatabaseState?.cid == nil {
+                DispatchQueue.main.async {
+                    self?.showNoDBAlert()
+                }
             } else {
                 // DB found, fetch DB
+                
             }
         }
     }
     
-    func createNewDB() {
+    func createNewDatabase() {
+        
+        NetworkManager.sharedManager.uploadDatabase { (success, error) in
+            
+            //            switch result {
+            //            case .success(let receivedFileData):
+            //                print("Uploaded successfully, received response:")
+            //                print("File Location: \(receivedFileData.cid)")
+            //            case .failure(let error):
+            //                print("Failed to upload config: \(error)")
+            //            }
+        }
         
     }
 }
@@ -83,9 +68,8 @@ extension ViewController {
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
         
         let confirmAction = UIAlertAction(title: "Yes, Go Ahead", style: .default) { _ in
-            
             // create and upload an empty DB file
-            self.createNewDB()
+            self.createNewDatabase()
         }
         alert.addAction(cancelAction)
         alert.addAction(confirmAction)
