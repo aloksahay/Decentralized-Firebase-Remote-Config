4a58918e5fd5ff45f4bf994c79eb1d007eb97319
Alok Sahay <alok.sahay87@gmail.com>
2024-10-14 00:36:31 +0200

    Clear

--- RemoteConfigUsageExample/RemoteConfigUsageExample/AppDelegate.swift
+++ RemoteConfigUsageExample/RemoteConfigUsageExample/AppDelegate.swift
@@ -10,11 +10,9 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-    
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        RemoteAppConfig.setupSharedInstance(withURL: "https://cyan-genetic-barracuda-339.mypinata.cloud/files/bafkreib7texbtz62bsihxto67fyrh7bf237ij3e6kz65wsytw6eu3dau5m?X-Algorithm=PINATA1&X-Date=1728857932014&X-Expires=5000000&X-Method=GET&X-Signature=193547a3a82bf0e4270f2eb344f0d9026bd5e3b0d76474096e61281d352aa536")
+        RemoteAppConfig.setupSharedInstance(withURL: "")
         return true
     }
     
